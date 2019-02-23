8f57f9103bdee17ca32315e4229a4c229efc49f6
Myles Voigt <myles.voigt@gmail.com>
2019-02-23 11:25:10 -0600

    update_record() added

--- DbManager.py
+++ DbManager.py
@@ -13,7 +13,7 @@ cur = db.cursor()
 cur.execute('create table if not exists venue(Venue text, Date date, Street text, State text, ZipCode text)')
 cur.execute('create table if not exists merchandise(ItemName text, Description text, Price float, Quantity integer)')
 cur.execute('create table if not exists sales(Venue text, ItemName text, NumItems integer, '
-            'FOREIGN KEY(Venue) REFERENCES venue(venue), FOREIGN KEY(ItemName) REFERENCES merchandise(ItemName))')
+            'FOREIGN KEY(Venue) REFERENCES venue(Venue), FOREIGN KEY(ItemName) REFERENCES merchandise(ItemName))')
 
 #prints menu selections and gets/returns selection
 def main_menu_print():
@@ -53,12 +53,12 @@ def add_record(choice):
         db.commit()
     elif choice == 2:
         #get new merchandise info
-        item = input('What is the item name? ')
+        itemName = input('What is the item name? ')
         description = input('Enter a description of the item. ')
         price = float(input('Enter a price for the item. '))
         quantity = int(input('How many of these do you have? '))
         #add record to database
-        cur.execute('insert into merchandise values(?, ?, ?, ?)', (item, description, price, quantity))
+        cur.execute('insert into merchandise values(?, ?, ?, ?)', (itemName, description, price, quantity))
         #commit the changes
         db.commit()
     elif choice == 3:
@@ -71,7 +71,7 @@ def add_record(choice):
         #commit changes
         db.commit()
 
-#method used to update records
+#method used to update records. Allows you to build an sql string.
 def update_record(choice):
     #determine which table to update
     tableName = ''
@@ -84,7 +84,7 @@ def update_record(choice):
     #get update information
     update = input('What field would you like to update? ')
     newValue = input('Enter a new value for this field. ')
-    crit = input('Any criteria for this update? (y/n)')
+    crit = input('Is there any criteria for this update? (y/n)')
     if crit.upper() == 'N':
         #update information
         cur.execute('update ? set ? = ?', (tableName, update, newValue))
@@ -92,17 +92,34 @@ def update_record(choice):
         db.commit()
     elif crit.upper() == 'Y':
         #get criteria
-        criteria = input('Enter the first field. ')
-        operator = input('Enter the criteria (column1 <,>,=, or <> column2)')
-        criteria2 = input('Enter the second field. ')
+        print('You must enter criteria in this format: (column1 <,>,=, or <> column2)')
+        criteria_1 = input('Enter column1. ')
+        operator = input('Enter the criteria (<,>,=, or <>)')
+        criteria_2 = input('Enter column2. ')
         #update the record
-        cur.execute("update ? set ? = ? where ?' '?' '?",(tableName, update, newValue, criteria, operator, criteria2))
+        cur.execute("update ? set ? = ? where ?' '?' '?",(tableName, update, newValue, criteria_1, operator, criteria_2))
         #commit changes
         db.commit()
 
-#method used to delete records
+#method used to delete records. Only allows you to delete with criteria.
 def delete_record(choice):
-    return
+    # determine which table to update
+    tableName = ''
+    if choice == 1:
+        tableName = 'venue'
+    elif choice == 2:
+        tableName = 'merchandise'
+    elif choice == 3:
+        tableName = 'sales'
+    # get update information
+    print('You must enter criteria in this format: (column1 <,>,=, or <> column2)')
+    criteria_1 = input('Enter column1. ')
+    operator = input('Enter the criteria (<,>,=, or <>)')
+    criteria_2 = input('Enter column2. ')
+    #delete the record
+    cur.execute("delete from ? where ?' '?' '?", (tableName, criteria_1, operator, criteria_2))
+    #commit changes
+    db.commit()
 
 #main method
 def main():
